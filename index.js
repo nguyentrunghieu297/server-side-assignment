d819b7908e0842d1b1636fbeb7c1261a4e1bb069
nguyentrunghieu297 <trunghieunguyen2729@gmail.com>
2024-06-17 11:31:21 +0700

    Second commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGODB)
   .then(() => {
     console.log('MongoDB connected successfully');
   })
